82d580a35829b3c537c9ccdb23fb269d0efb2842
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-12-09 12:04:26 +0530

    FIX : news query

--- src/pages/api/rest/news.ts
+++ src/pages/api/rest/news.ts
@@ -16,10 +16,12 @@ type Article={
 }
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data | ErrorResponse>) {
   try{
-    if (req.method === "GET" && !req.query.type) {
+    // const val=req.query.type;
+    // console.log({val});
+    if (req.method === "GET" && req.query.type===undefined) {
       res.status(200).json({ status:"ok",totalResults:News.articles.length,data:News });
     } 
-    if (req.method === "GET" && req.query.type) {
+    if (req.method === "GET" && req.query.type!=undefined) {
       // const filter=req.query.type;
       const filterdata:any =ModifiedNews.filter((ele)=>ele.type===req.query.type);
       res.status(200).json({ status:"ok",totalResults:filterdata[0].articles.length?filterdata[0].articles.length:0,data:filterdata[0].articles });
